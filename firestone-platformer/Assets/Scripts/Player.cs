f9e26759bb7adef3a81d015c985e2b9d774528b1
Firestone <maddyfirestone@gmail.com>
2024-02-01 12:27:42 -0800

    Added new files back to repo

---
+++ firestone-platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float runSpeed =5.0f;
+    [SerializeField] private float jumpSpeed = 5.0f;
+    [SerializeField] private float climbSpeed = 5.0f;
+
+    private float gravityScaleAtStart;
+
+
+    Rigidbody2D playerCharacter;
+
+    Animator playerAnimator;
+
+    Collider2D playerCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCharacter = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<Collider2D>();
+
+        gravityScaleAtStart = playerCharacter.gravityScale;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        Jump();
+        Climb();
+    }
+
+    private void Run()
+    {
+        //value between -1 and 1
+        float hMovement = Input.GetAxis("Horizontal");
+        Vector2 runVelocity = new Vector2(hMovement * runSpeed, playerCharacter.velocity.y);
+        playerCharacter.velocity = runVelocity;
+
+        //playerAnimator.SetBool("run", true);
+         
+        print(runVelocity);
+
+        bool hSpeed = Mathf.Abs(playerCharacter.velocity.x) > Mathf.Epsilon;
+
+        playerAnimator.SetBool("run", hSpeed);
+
+    }
+
+    private void FlipSprite()
+    {
+        //if player is moving horixontally
+        bool hMovement = Mathf.Abs(playerCharacter.velocity.x) > Mathf.Epsilon;
+
+        if (hMovement)//reverse the current direction of the x-axis
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerCharacter.velocity.x), 1f);
+        } 
+      
+    }
+
+    private void Jump()
+    {
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask ("Ground")))
+        {
+            //Will stop this function unless true
+            return;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            // Get new Y by velocity based on a variable 
+            Vector2 jumpVelocity = new Vector2(0.0f, jumpSpeed);
+            playerCharacter.velocity += jumpVelocity;
+        }
+    }
+
+    private void Climb()
+    {
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            playerAnimator.SetBool("climb", false);
+            playerCharacter.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        //"Vertical" from Input Axis
+        float vMovement = Input.GetAxis("Vertical");
+
+        //X axis needs to remain the same and we nbeed to change the Y
+        Vector2 climbVelocity = new Vector2(playerCharacter.velocity.x, vMovement * climbSpeed);
+        playerCharacter.velocity = climbVelocity;
+
+        playerCharacter.gravityScale = 0.0f;
+
+        bool vSpeed = Mathf.Abs(playerCharacter.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("climb", vSpeed);
+    }
+}
